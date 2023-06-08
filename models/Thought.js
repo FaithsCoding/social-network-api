c7be18976bd5530430dbfdfcfe910546c9e61758
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-06-08 12:31:41 +0100

    completed thought and user models

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,76 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      //ObjectID data type set out by Mongoose
+      type: Schema.Types.ObjectId,
+      //default will be set to new ObjectId
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      //sets timestamp to show what the time is now
+      default: Date.now,
+      //getter method is used to format it
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Thought is required",
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mode("Thought", ThoughtSchema);
+
+module.exports = Thought;
